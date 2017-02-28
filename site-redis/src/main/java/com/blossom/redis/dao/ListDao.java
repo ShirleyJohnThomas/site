008b18f0f6478ba2f80f5966ad65e7af13816476
blossom <blossom_zhang@163.com>
2017-02-28 19:07:13 +0800

    list type operation in redis moducl

--- site-redis/src/main/java/com/blossom/redis/dao/ListDao.java
+++ site-redis/src/main/java/com/blossom/redis/dao/ListDao.java
@@ -52,4 +52,70 @@ public class ListDao {
 	public void rsave(byte[] key,byte[]...values){
 		JedisUtils.getJedis().rpush(key, values);
 	}
+	
+	/**
+	 * @Description: 将列表最后一个元素弹出，并返回给客户端
+	 * @author Blossom
+	 * @time 2017年2月28日 下午6:57:50 
+	 * @return_type void
+	 *
+	 */
+	public void rget(String srckey,String dstkey){
+		JedisUtils.getJedis().rpoplpush(srckey, dstkey);
+	}
+	
+	/**
+	 * @Description: 将列表最后一个元素弹出，并返回给客户端
+	 * @author Blossom
+	 * @time 2017年2月28日 下午6:57:50 
+	 * @return_type void
+	 *
+	 */
+	public void rget(byte[] srckey,byte[] dstkey){
+		JedisUtils.getJedis().rpoplpush(srckey, dstkey);
+	}
+	
+	/**
+	 * @Description: 获取队列数据(顺序)
+	 * @author Blossom
+	 * @time 2017年2月28日 下午6:59:57 
+	 * @return_type byte[]
+	 *
+	 */
+	public byte[] getList(byte[] key){
+		return JedisUtils.getJedis().lpop(key);
+	}
+	
+	/**
+	 * @Description: 获取队列数据(顺序)
+	 * @author Blossom
+	 * @time 2017年2月28日 下午7:00:49 
+	 * @return_type String
+	 *
+	 */
+	public String getList(String key){
+		return JedisUtils.getJedis().lpop(key);
+	}
+	
+	/**
+	 * @Description:获取队列数据(反向) 
+	 * @author Blossom
+	 * @time 2017年2月28日 下午7:01:45 
+	 * @return_type String
+	 *
+	 */
+	public String rgetList(String key){
+		return JedisUtils.getJedis().rpop(key);
+	}
+	
+	/**
+	 * @Description:获取队列数据(反向)  
+	 * @author Blossom
+	 * @time 2017年2月28日 下午7:02:44 
+	 * @return_type byte[]
+	 *
+	 */
+	public byte[] rgetList(byte[] key){
+		return JedisUtils.getJedis().rpop(key);
+	}
 }
